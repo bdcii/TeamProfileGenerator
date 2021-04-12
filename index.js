eee24829a1c412f6714338ecaffb395062d36bce
bdcii <darrell.chess@gmail.com>
2021-04-12 16:52:14 -0400

    updated index.js to point where user can cycle through prompts and data is collected

--- index.js
+++ index.js
@@ -9,114 +9,126 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+//array that collects employee data from user submission
+let teamArray = [];
 
+//function that runs at start off application
+function runApp() {
+    console.log('Please enter Manager information to begin building team');
+    addManager();
+}
 
+//function that runs at end of each team member prompt. Lets user choose next step to take.
+function askWhatsNext() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do next?',
+            choices: ['Add an Engineer', 'Add an Intern', 'Finish building My Team'],
+            name: 'menuOption',
+        }
+    ]).then(answers => {
+        if (answers.menuOption === 'Add an Engineer') {
+            addEngineer()
+        } else if (answers.menuOption === 'Add an Intern') {
+            addIntern()
+        } else buildTeam()
+    })
+}
 
+//function that prompts user for Manager data.
+function addManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the Team Manager name?',
+            name: 'managerName',
+        },
+        {
+            type: 'input',
+            message: 'What is the Team Manager employee ID?',
+            name: 'managerID',
+        },
+        {
+            type: 'input',
+            message: 'What is the Team Manager email address?',
+            name: 'managerEmail',
+        },
+        {
+            type: 'input',
+            message: 'What is the Team Manager office number?',
+            name: 'managerOffice',
+        },
+    ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOffice);
+        teamArray.push(manager);
+        askWhatsNext();
+    })
+}
 
+//function that prompts user for engineer data
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the Engineer?',
+            name: 'engineerName',
+        },
+        {
+            type: 'input',
+            message: 'What is the Engineer ID?',
+            name: 'engineerID',
+        },
+        {
+            type: 'input',
+            message: 'What is the Enginneer email address?',
+            name: 'engineerEmail',
+        },
+        {
+            type: 'input',
+            message: 'What is Engineers GitHub username?',
+            name: 'engineerGitHub',
+        },
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub);
+        teamArray.push(engineer);
+        askWhatsNext();
+    })
+}
 
+//function that prompts user for intern data
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the Intern?',
+            name: 'internName',
+        },
+        {
+            type: 'input',
+            message: 'What is the Intern ID?',
+            name: 'internID',
+        },
+        {
+            type: 'input',
+            message: 'What is the Intern email address?',
+            name: 'internEmail',
+        },
+        {
+            type: 'input',
+            message: 'Where does the Intern attend school?',
+            name: 'internSchool',
+        },
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+        teamArray.push(intern);
+        askWhatsNext();
+    })
+}
 
+function buildTeam(){
+    console.log('Team Members: ', teamArray);
+}
 
-inquirer.prompt([
-    {
-        type: 'input',
-        message: 'What is the Team Manager name?',
-        name: 'managerName',
-    },
-    {
-        type: 'input',
-        message: 'What is the Team Manager employee ID?',
-        name: 'managerID',
-    },
-    {
-        type: 'input',
-        message: 'What is the Team Manager email address?',
-        name: 'managerEmail',
-    },
-    {
-        type: 'input',
-        message: 'What is the Team Manager office number?',
-        name: 'managerOffice',
-    },
-
-
-    {
-        type: 'list',
-        message: 'What would you like to do next?',
-        choices: ['Add an Engineer', 'Add an Intern', 'Finish building My Team'],
-        name: 'menuOption',
-    },
-
-
-    {
-        type: 'input',
-        message: 'What is the name of the Engineer?',
-        name: 'engineerName',
-        when: (answers) => answers.menuOption === 'Add an Engineer'
-    },
-    {
-        type: 'input',
-        message: 'What is the Engineer ID?',
-        name: 'engineerID',
-    },
-    {
-        type: 'input',
-        message: 'What is the Enginneer email address?',
-        name: 'engineerEmail',
-    },
-    {
-        type: 'input',
-        message: 'What is Engineers GitHub username?',
-        name: 'engineerGitHub',
-    },
-
-
-    {
-        type: 'list',
-        message: 'What would you like to do next?',
-        choices: ['Add an Engineer', 'Add an Intern', 'Finish building My Team'],
-        name: 'menuOption',
-    },
-
-
-    {
-        type: 'input',
-        message: 'What is the name of the Intern?',
-        name: 'internName',
-        when: (answers) => answers.menuOption === 'Add an Intern'
-    },
-    {
-        type: 'input',
-        message: 'What is the Intern ID?',
-        name: 'internID',
-    },
-    {
-        type: 'input',
-        message: 'What is the Intern email address?',
-        name: 'internEmail',
-    },
-    {
-        type: 'input',
-        message: 'Where does the Intern attend school?',
-        name: 'internSchool',
-    },
-
-    {
-        type: 'list',
-        message: 'What would you like to do next?',
-        choices: ['Add an Engineer', 'Add an Intern', 'Finish building My Team'],
-        name: 'menuOption',
-    },
-
-
-])
-
-.then((answers) =>{
-
-    const {managerName, managerID, managerEmail, managerOffice, menuOption, engineerName, engineerID, engineerEmail, engineerGitHub, internName, internID, internEmail, internSchool } = answers;
-
-
-
-
-//will need function to generate HTML file containing data collected from prompts
-
-})
+//starting application
+runApp();
